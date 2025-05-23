1fa400b9951336bff2ef2077bfc2083fdf223f98
Alok Hom <alok.hom@gmail.com>
2025-05-23 23:10:42 +0200

    add

--- go/hello.go
+++ go/hello.go
@@ -2,47 +2,97 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"sort"
+	"time"
 )
 
+const url = "https://services.explorecalifornia.org/json/tours.php"
+
 func main() {
-	firstName := "Bob"
-	familyName := "Smith"
-	age := 34
-	peanutAllergy := false
 
-	fmt.Println(firstName)
-	fmt.Println(familyName)
-	fmt.Println(age)
-	fmt.Println(peanutAllergy)
+	t := time.Now()
+	fmt.Printf("Current time: %s\n", t.Format(time.RFC1123))
+	fmt.Printf("Current time: %s\n", t.Format(time.ANSIC))
 
-	a, b := 5, 10
-	swap(&a, &b)
-	fmt.Println(a == 10, b == 5)
+	tomorrow := t.Add(24 * time.Hour)
+	fmt.Printf("Current time: %s\n", tomorrow)
 
-	count := 5
-	var message string
-	if count > 5 {
-		message = "Greater than 5"
-	} else {
-		message = "Not greater than 5"
+	tFormat := t.Format("Mon 2006-01-02 15:04:05")
+	fmt.Printf("Current time: %s\n", tFormat)
+
+	var colours [3]string
+	colours[0] = "red"
+	colours[1] = "green"
+	colours[2] = "blue"
+	fmt.Printf("Colours: %v\n", colours)
+
+	var numbers = [3]int{1, 2, 3}
+	fmt.Printf("Numbers: %v\n", numbers)
+
+	//slice
+	var colours1 = make([]string, 0, 3)
+	colours1 = append(colours1, "red")
+	colours1 = append(colours1, "green")
+	fmt.Printf("Colours1: %v\n", colours1)
+
+	colours1 = remove(colours1, 0)
+	fmt.Printf("Colours1: %v\n", colours1)
+
+	sort.Strings(colours1)
+	fmt.Println(colours1)
+
+	// map
+	states := make(map[string]string)
+	states["CA"] = "California"
+	states["NY"] = "New York"
+	states["TX"] = "Texas"
+	fmt.Printf("States: %v\n", states)
+	for k, v := range states {
+		fmt.Printf("Key: %s, Value: %s\n", k, v)
 	}
-	fmt.Println(message)
 
-	count1 := 0
-	if count1 < 5 {
-		count1 = 10
-		count1++
+	//slice
+	keys := make([]string, len(states))
+	i := 0
+	for k := range states {
+		keys[i] = k
+		i++
 	}
-	fmt.Println(count1 == 11)
+	sort.Strings(keys)
+	fmt.Println("\nSorted")
 
-	http.HandleFunc("/hello", func(rw http.ResponseWriter, req *http.Request) {
-		name := req.URL.Query().Get("name")
-		rw.Write([]byte(fmt.Sprintf("Hello, %s", name)))
-	})
-	http.ListenAndServe(":8080", nil)
+	for i := range keys {
+		fmt.Println(states[keys[i]])
+	}
+
+	//http
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	checkErr(err)
+	req.Header.Set("User-Agent", "")
+	resp, err := client.Do(req)
+	checkErr(err)
+	defer resp.Body.Close()
+	fmt.Printf("Response type: %T\n", resp)
+	bytes, err := io.ReadAll(resp.Body)
+	checkErr(err)
+	content := string(bytes)
+	fmt.Print(content)
+
 }
 
-func swap(a *int, b *int) {
-	*a, *b = *b, *a
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
+
+func remove(slice []string, i int) []string {
+	return append(slice[:i], slice[i+1:]...)
+}
+
+// func swap(a *int, b *int) {
+// 	*a, *b = *b, *a
+// }
